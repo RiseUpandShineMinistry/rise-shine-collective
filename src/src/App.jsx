67b6b41ca620a40fca2b382a1f6c37a8d2e7a8bf
RiseUpandShineMinistry <victory2wholeness@gmail.com>
2026-03-11 15:08:42 -0400

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{fontFamily:"Arial", textAlign:"center", marginTop:"80px"}}>
+      <h1>Rise Up & Shine Ministry</h1>
+      <p>Faith. Healing. Freedom.</p>
+      <p>You’ve carried others. Now it’s time to care for you.</p>
+    </div>
+  )
+}
